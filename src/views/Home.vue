a2d54d00155fb1d5720a68baa52a88f7c513af86
Pierina Montalva <pierinamfatur@gmail.com>
2021-11-09 00:15:53 -0500

    function getProductsColl

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,12 +8,35 @@
 <script>
 import Nav from '../components/Nav';
 import SearchInput from '../components/SearchInput.vue';
+import querySnapshot from '../firebase/firestore';
+import { ref, onMounted } from 'vue';
 
 export default {
   name: 'Home',
   components: {
     Nav,
     SearchInput,
+  },
+  setup() {
+    const products = ref([]);
+
+    const getProductsColl = async () => {
+    const dataProducts = await querySnapshot('productosDos');
+    products.value = dataProducts.docs.map((doc) => (
+     {
+       brand: doc.brand,
+       name: doc.name,
+       price: doc.price,
+       suggestedPrice: doc.suggestedPrice,
+    }))
+    };
+
+    onMounted(getProductsColl);
+    
+    return {
+      products,
+      getProductsColl,
+    }
   }
 }
 </script>
